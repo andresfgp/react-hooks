898097c4003ce68ee3621617116b45d53a5857f0
Andres Garcia <andres.fgp@hotmail.com>
2022-09-28 14:35:24 -0500

    useCallback and create custom hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import ThemeContext from '../context/ThemeContext'
+import React, { useState, useContext } from 'react';
+import ThemeContext from '../context/ThemeContext';
 
 const Header = () => {
-    const [darkMode, setDarkMode] = React.useState(false);
-    const color = React.useContext(ThemeContext);
+  const [darkMode, setDarkMode] = useState(false);
+  const color = useContext(ThemeContext);
 
-    const handleClick = () => {
-        setDarkMode(!darkMode);
-    }
+  const handleClick = () => {
+    setDarkMode(!darkMode);
+  }
 
-    return (
-        <div className="header">
-            <h1 style={{color}}>React Hooks</h1>
-            <button type="button" onClick={handleClick}>{darkMode ? "Dark Mode" : "Light Mode"}</button>
-        </div>
-    );
-};
+  return (
+    <div className="Header">
+      <h1 style={{ color }}>ReactHooks</h1>
+      <button type="button" onClick={handleClick}>{darkMode ? 'Dark Mode' : 'Light Mode'}</button>
+    </div>
+  );
+}
 
 export default Header;
